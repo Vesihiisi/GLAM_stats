64b665ba4492e87b0625cf7be4ede96833066780
alicia <alicia@fagerving.se>
2017-12-01 18:55:14 +0100

    first

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # GLAM_stats
-grab glamorous stats for selected categories
+Python script to grab [glamorous](https://tools.wmflabs.org/glamtools/glamorous.php) stats for selected categories in Wikimedia Commons.
+
+Saves result as json, wikitable and markdown files (like this one):
+
+| institution | files | used |
+|---|---|---|
+| [Riksantikvarieämbetet](https://commons.wikimedia.org/wiki/Category:Images_from_the_Swedish_National_Heritage_Board) | 185636 | 2246 |
+| [Livrustkammaren, Skoklosters slott och Hallwylska museet](https://commons.wikimedia.org/wiki/Category:Images_from_Livrustkammaren_och_Skoklosters_slott_med_Stiftelsen_Hallwylska_museet) | 46332 | 1760 |
+| [Nationalmuseum](https://commons.wikimedia.org/wiki/Category:Images_from_the_Nationalmuseum_Stockholm) | 5401 | 4609 |
+| [Musikverket](https://commons.wikimedia.org/wiki/Category:Images_from_the_Swedish_Performing_Arts_Agency) | 4554 | 68 |
+| [Nordiska museet](https://commons.wikimedia.org/wiki/Category:Images_from_Nordiska_museet) | 2900 | 531 |
+| [Kungliga Biblioteket](https://commons.wikimedia.org/wiki/Category:Images_from_the_National_Library_of_Sweden) | 2525 | 27 |
+| [Statens museer för världskultur](https://commons.wikimedia.org/wiki/Category:Media_from_the_National_Museums_of_World_Culture) | 1908 | 98 |
+| [Statens maritima museer](https://commons.wikimedia.org/wiki/Category:Images_from_Statens_maritima_museer) | 1003 | 225 |
+| [Tekniska museet](https://commons.wikimedia.org/wiki/Category:Images_from_Tekniska_museet) | 987 | 160 |
+| [Riksarkivet](https://commons.wikimedia.org/wiki/Category:Images_from_the_National_Archives_of_Sweden) | 677 | 84 |
+| [Work With Sounds (Arbetets museum)](https://commons.wikimedia.org/wiki/Category:Media_from_Work_With_Sounds) | 652 | 43 |
+| [Spårvägsmuseet](https://commons.wikimedia.org/wiki/Category:Images_from_Spårvägsmuseet) | 247 | 59 |
+| [Regionarkivet](https://commons.wikimedia.org/wiki/Category:Images_from_Regionarkivet) | 74 | 24 |
+| [Brunnsmuseet](https://commons.wikimedia.org/wiki/Category:Media_provided_by_Brunnsmuseet) | 50 | 7 |
+| [Internetmuseum](https://commons.wikimedia.org/wiki/Category:Images_from_Internetmuseum) | 40 | 9 |
+| [Riksutställningar](https://commons.wikimedia.org/wiki/Category:Images_from_Riksutställningar) | 37 | 0 |
+| [Stockholms stadsmuseum](https://commons.wikimedia.org/wiki/Category:Images_from_Stockholms_stadsmuseum) | 2 | 1 |
+
+## Usage
+
+You need a json file with the categories you want to analyze. See `se_config.json` for what it needs to look like. Then you pass it to the script:
+
+```bash
+$ python3 glam_stats.py --config cats.json
+```
+
+## Acknowledgments
+* Based on [Swedish-GLAMwiki-stats](https://github.com/Ambrosiani/Swedish-GLAMwiki-stats) by [Aron Ambrosiani](https://github.com/Ambrosiani)
+* Spiritual support by [this cat's upper teethsies](https://commons.wikimedia.org/wiki/File:Cat_teeth.jpg). Have you ever *really* looked at a cat's upper teethsies?
---
+++ glam_stats.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+from datetime import datetime
+from lxml import etree
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Save usage stats for selected GLAM categories.
+
+Usage: python3 glam_stats.py --config cats.json
+"""
+
+import argparse
+import json
+import requests
+
+
+def generate_filename():
+    return datetime.today().strftime('%Y-%m-%d')
+
+
+def make_url(catname):
+    base = (
+        "https://tools.wmflabs.org//glamtools/glamorous.php"
+        "?doit=1&category={}"
+        "&use_globalusage=1&ns0=1&depth=9&projects[wikipedia]=1"
+        "&projects[wikimedia]=1&projects[wikisource]=1"
+        "&projects[wikibooks]=1&projects[wikiquote]=1"
+        "&projects[wiktionary]=1&projects[wikinews]=1"
+        "&projects[wikivoyage]=1&projects[wikispecies]=1"
+        "&projects[mediawiki]=1&projects[wikidata]=1"
+        "&projects[wikiversity]=1&format=xml"
+    )
+    return base.format(catname.replace(" ", "+"))
+
+
+def process_data(xmlblob):
+    row = {}
+    tree = etree.fromstring(xmlblob)
+    row["files"] = tree.get("images_in_category")
+    for child_of_root in tree:
+        row["total_usage"] = child_of_root.get("total_usage")
+        row["distinct_used"] = child_of_root.get("distinct_images")
+    return row
+
+
+def analyze_single(item):
+    url = make_url(item["cat"])
+    data = download_data(url)
+    row = process_data(data)
+    row["name"] = item["name"]
+    row["cat"] = item["cat"].replace(" ", "_")
+    return row
+
+
+def download_data(url):
+    return requests.get(url).text
+
+
+def read_json_file(fname):
+    return json.load(open(fname))
+
+
+def save_file(fname, txt):
+    with open(fname, 'w') as f:
+        f.write(txt)
+        print("Saved file: {}".format(fname))
+
+
+def save_json_file(fname, data):
+    with open(fname, 'w') as f:
+        json.dump(data, f,
+                  ensure_ascii=False,
+                  sort_keys=True, indent=4)
+        print("Saved file: {}".format(fname))
+
+
+def make_wikitable(data):
+    txt = '{| class="wikitable"\n|-\n'
+    txt += "! {}\n".format("institution")
+    txt += "! {}\n".format("files")
+    txt += "! {}\n".format("used")
+    for r in data:
+        link_base = "[[:commons:Category:{}|{}]]"
+        link = link_base.format(r["cat"], r["name"])
+        txt += "|-\n"
+        txt += "| {}\n".format(link)
+        txt += "| {}\n".format(r["files"])
+        txt += "| {}\n".format(r["distinct_used"])
+    txt += "|}\n"
+    return txt
+
+
+def make_markdown(data):
+    txt = "| {} | {} | {} |\n".format("institution",
+                                      "files",
+                                      "used")
+    txt += "|---|---|---|\n"
+    for r in data:
+        link_base = "https://commons.wikimedia.org/wiki/Category:{}"
+        link_str = link_base.format(r["cat"])
+        link = "[{}]({})".format(r["name"], link_str)
+        txt += "| {} | {} | {} |\n".format(link,
+                                           r["files"],
+                                           r["distinct_used"])
+    return txt
+
+
+def extract_number_files(json):
+    return int(json["files"])
+
+
+def save_processed(rows):
+    fname = generate_filename()
+    save_json_file("{}.json".format(fname), rows)
+    save_file("{}.md".format(fname), make_markdown(rows))
+    save_file("{}.wiki".format(fname), make_wikitable(rows))
+
+
+def main(f_config):
+    cats = read_json_file(f_config)
+    message = "[{}] I'm going to analyze {} cats...🐈"
+    print(message.format(f_config, len(cats)))
+    rows = []
+    for cat in cats:
+        rows.append(analyze_single(cat))
+    rows.sort(key=extract_number_files, reverse=True)
+    save_processed(rows)
+
+
+if __name__ == "__main__":
+    arguments = {}
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config",
+                        help='file with categories to analyze',
+                        default="se_config.json")
+    args = parser.parse_args()
+    main(args.config)
